dd83e65d36fd4c7ea72eea3a9c0962a3b62d5420
2550286303@qq.com <2550286303@qq.com>
2016-09-12 15:21:02 +0800

    添加了获取网页中电话号码的代码，优化了webview页面，点击返回键返回webview上级页面

--- app/src/main/java/com/example/administrator/loanapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/loanapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.administrator.loanapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
    private WebView webView;
+    private static final String TAG = "MainActivity";
     //http://bk.0531center.com/
     private static  final String WEBURL="http://bk.0531center.com/";
     @Override
@@ -14,14 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = (WebView) findViewById(R.id.wv);
+        //webview支持js
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(WEBURL);
+        //在webview中打开一个新的页面
         webView.setWebViewClient(new WebViewClient() {
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                //  重写此方法表明点击网页里面的链接还是在当前的webview里跳转，不跳到浏览器那边
-                view.loadUrl(url);
+         public boolean   shouldOverrideUrlLoading(WebView view, String url){
+             //判断url是否有tel，截取电话号码。开启打电话的意图
+             if(url != null&&"tel".equals(url.substring(0,3))){
+                 Intent intent=new Intent(Intent.ACTION_CALL, Uri.parse(url));
+                 startActivity(intent);
+                 return true;
+             }
                 return false;
-            }
-        });
+        }});
+}
+    ///重写webview 的onKeyDown方法覆盖系统的onKeyDown方法
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack(); // goBack()表示返回WebView的上一页面
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
     }
 }
