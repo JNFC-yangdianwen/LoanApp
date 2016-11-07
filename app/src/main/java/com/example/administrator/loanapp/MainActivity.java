b25ff92ad705a81a9dbe4ad3b236274814223946
Administrator <2550286303@qq.com>
2016-11-07 14:39:08 +0800

    实现了js确认框

--- app/src/main/java/com/example/administrator/loanapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/loanapp/MainActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
    private WebView webView;
+    private static int type=1;
     private static final String TAG = "MainActivity";
     //http://bk.0531center.com/
     private static  final String WEBURL="http://gy.0531center.com/";
@@ -35,7 +40,33 @@ public class MainActivity extends AppCompatActivity {
              }
                 return false;
         }});
+     webView.setWebChromeClient(new WebChromeClient(){
+         //确定
+         @Override
+         public boolean onJsAlert(WebView view, String url, String message,
+                                  JsResult result) {
+             // TODO Auto-generated method stub
+             Log.e("info", "onJsAlert message:"+message);
+             return super.onJsAlert(view, url, message, result);
+         }
+          //获取到的信息
+         @Override
+         public boolean onJsConfirm(WebView view, String url,
+                                    String message, JsResult result) {
 
+             // TODO Auto-generated method stub
+             Log.e("info", "onJsConfirm url"+url);
+             return super.onJsConfirm(view, url, "", result);
+         }
+
+         @Override
+         public boolean onJsPrompt(WebView view, String url, String message,
+                                   String defaultValue, JsPromptResult result) {
+             // TODO Auto-generated method stub
+             Log.e("info", "onJsPrompt message:"+message);
+             return super.onJsPrompt(view, url, message, defaultValue, result);
+         }
+     });
 }
     ///重写webview 的onKeyDown方法覆盖系统的onKeyDown方法
     public boolean onKeyDown(int keyCode, KeyEvent event) {
